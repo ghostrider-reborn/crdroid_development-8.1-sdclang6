042ba21b8769f419fca639d21689eb6f4ee26f48
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-07-19 17:50:50 +0530

    soong: cc: Simplify sdclang logic
    * instead of reading them from a stupid json file, just read it from the env
    
    * makes life easier for the builder too

--- cc/makevars.go
+++ cc/makevars.go
@@ -133,7 +133,7 @@ func makeVarsProvider(ctx android.MakeVarsContext) {
 	if config.SDClang {
 		ctx.Strict("SDCLANG", strconv.FormatBool(config.SDClang))
 		ctx.Strict("SDCLANG_PATH", "${config.SDClangBin}")
-		ctx.Strict("SDCLANG_COMMON_FLAGS", "${config.SDClangFlags}")
+		ctx.Strict("SDCLANG_FLAGS", "${config.SDClangFlags}")
 	}
 }
 
